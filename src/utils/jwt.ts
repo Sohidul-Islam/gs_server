eb1e8bb91cacc3f1289df90fdf7428248d1d5adf
Sohidul-Islam <sishufol.sim@gmail.com>
2025-07-19 11:41:49 +0600

    Add JWT utility functions and types for token generation and verification

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -3,3 +3,60 @@ import jwt from "jsonwebtoken";
 export function verifyJwt(token: string): any {
   return jwt.verify(token, process.env.JWT_SECRET || "your_jwt_secret");
 }
+
+export interface JwtPayload {
+  id: number;
+  email: string;
+  username: string;
+  role?: string;
+}
+
+type Unit =
+| "Years"
+| "Year"
+| "Yrs"
+| "Yr"
+| "Y"
+| "Weeks"
+| "Week"
+| "W"
+| "Days"
+| "Day"
+| "D"
+| "Hours"
+| "Hour"
+| "Hrs"
+| "Hr"
+| "H"
+| "Minutes"
+| "Minute"
+| "Mins"
+| "Min"
+| "M"
+| "Seconds"
+| "Second"
+| "Secs"
+| "Sec"
+| "s"
+| "Milliseconds"
+| "Millisecond"
+| "Msecs"
+| "Msec"
+| "Ms";
+
+type UnitAnyCase = Unit | Uppercase<Unit> | Lowercase<Unit>;
+
+type StringValue =
+    | `${number}`
+    | `${number}${UnitAnyCase}`
+    | `${number} ${UnitAnyCase}`;
+
+export function generateJwtToken(payload: JwtPayload, expiresIn: (StringValue|number) = "1h"): string {
+  return jwt.sign(
+    { ...payload, expiresIn },
+    process.env.JWT_SECRET || "your_jwt_secret",
+    {
+      expiresIn: expiresIn,
+    }
+  );
+}

5b4cf4deefdc9cd11afeb0aa7b0f44b298e2b8d3
Sohidul-Islam <sishufol.sim@gmail.com>
2025-07-20 15:18:49 +0600

    Add payment method type routes and seeding functionality, including schema exports for payment methods.

---
+++ src/routes/paymentMethodType.route.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import {
+  getAllPaymentMethodTypes,
+  getPaymentMethodTypeById,
+  createPaymentMethodType,
+  updatePaymentMethodType,
+  deletePaymentMethodType,
+} from "../controllers/paymentMethodType.controller";
+import { asyncHandler } from "../utils/asyncHandler";
+
+const router = Router();
+
+router.get("/", asyncHandler(getAllPaymentMethodTypes));
+router.get("/:id", asyncHandler(getPaymentMethodTypeById));
+router.post("/", asyncHandler(createPaymentMethodType));
+router.put("/:id", asyncHandler(updatePaymentMethodType));
+router.delete("/:id", asyncHandler(deletePaymentMethodType));
+
+export default router; 

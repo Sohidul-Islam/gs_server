bef4306ee5a32f2ecbf809d6a4d9d880d09d08eb
Sohidul-Islam <sishufol.sim@gmail.com>
2025-08-07 09:32:11 +0600

    Add primary account validation and update logic in PaymentGatewayProviderAccountController: implement checks to prevent multiple primary accounts for a provider, and ensure proper handling of existing accounts during creation and updates.

--- src/controllers/paymentGatewayProviderAccount.controller.ts
+++ src/controllers/paymentGatewayProviderAccount.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import { PaymentGatewayProviderAccountModel } from "../models/paymentGatewayProviderAccount.model";
 import { asyncHandler } from "../utils/asyncHandler";
+import { db } from "../db/connection";
+import { paymentGatewayProviderAccount } from "../db/schema";
+import { eq } from "drizzle-orm";
 
 export const PaymentGatewayProviderAccountController = {
   getAll: asyncHandler(async (req: Request, res: Response) => {
@@ -31,12 +34,10 @@ export const PaymentGatewayProviderAccountController = {
 
   getByProviderId: asyncHandler(async (req: Request, res: Response) => {
     const { paymentGatewayProviderId } = req.params;
-    console.log({ paymentGatewayProviderId });
     const data = await PaymentGatewayProviderAccountModel.getByProviderId(
       Number(paymentGatewayProviderId)
     );
 
-    console.log({ data });
     res.status(200).json({
       status: true,
       message: "Payment Gateway Provider Accounts fetched successfully",
@@ -45,6 +46,29 @@ export const PaymentGatewayProviderAccountController = {
   }),
 
   create: asyncHandler(async (req: Request, res: Response) => {
+    if (req.body?.isPrimary && req?.body?.is_primary === true) {
+      const existing = await PaymentGatewayProviderAccountModel.getByProviderId(
+        Number(req.body?.paymentGatewayProviderId)
+      );
+
+      if (existing && existing.length > 0) {
+        return res
+          .status(400)
+          .json({ status: false, message: "Primary account already exists" });
+      }
+
+      await db
+        .update(paymentGatewayProviderAccount)
+        .set({
+          isPrimary: false,
+        })
+        .where(
+          eq(
+            paymentGatewayProviderAccount.paymentGatewayProviderId,
+            Number(req.body?.paymentGatewayProviderId)
+          )
+        );
+    }
     const created = await PaymentGatewayProviderAccountModel.create(req.body);
     res.status(201).json({
       status: true,
@@ -55,6 +79,31 @@ export const PaymentGatewayProviderAccountController = {
 
   update: asyncHandler(async (req: Request, res: Response) => {
     const { id } = req.params;
+
+    const existing = await PaymentGatewayProviderAccountModel.getById(
+      Number(id)
+    );
+
+    if (!existing || existing.length === 0) {
+      return res
+        .status(404)
+        .json({ status: false, message: "Account not found" });
+    }
+
+    if (req.body?.isPrimary && req?.body?.isPrimary === true) {
+      await db
+        .update(paymentGatewayProviderAccount)
+        .set({
+          isPrimary: false,
+        })
+        .where(
+          eq(
+            paymentGatewayProviderAccount.paymentGatewayProviderId,
+            existing[0].paymentGatewayProviderId
+          )
+        );
+    }
+
     const updated = await PaymentGatewayProviderAccountModel.update(
       Number(id),
       req.body
